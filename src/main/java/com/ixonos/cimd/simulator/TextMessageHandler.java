ce9005c18d7ef4f45ea3510b5602ec88e33121ba
marko asplund <marko.asplund@ixonos.com>
2012-05-20 18:49:45 +0300

    use proper logging

--- src/main/java/com/ixonos/cimd/simulator/TextMessageHandler.java
+++ src/main/java/com/ixonos/cimd/simulator/TextMessageHandler.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.googlecode.jcimd.Packet;
 import com.googlecode.jcimd.Parameter;
@@ -19,6 +21,7 @@ import com.googlecode.jcimd.StringUserData;
  * @author Marko Asplund
  */
 public class TextMessageHandler extends IoHandlerAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(TextMessageHandler.class);
 	private Map<Long, IoSession> cimdSessions;
 	
 	public TextMessageHandler(Map<Long, IoSession> managedSessions) {
@@ -28,14 +31,14 @@ public class TextMessageHandler extends IoHandlerAdapter {
 	@Override
   public void messageReceived(IoSession session, Object message) throws Exception {
 		String msg = (String)message;
-		System.out.println("msg: "+msg);
+		logger.debug("msg: "+msg);
 		
 		for(Map.Entry<Long, IoSession> se : cimdSessions.entrySet()) {
 			String userId = (String)se.getValue().getAttribute("USER_ID");
 
 			List<Parameter> params = new LinkedList<Parameter>();
-			params.add(new Parameter(Parameter.DESTINATION_ADDRESS, "0407000216"));
-			params.add(new Parameter(Parameter.ORIGINATING_ADDRESS, "0405882407"));
+			params.add(new Parameter(Parameter.DESTINATION_ADDRESS, "040123"));
+			params.add(new Parameter(Parameter.ORIGINATING_ADDRESS, "040456"));
 			params.add(new Parameter(Parameter.MC_TIMESTAMP, new SimpleDateFormat("yyMMddHHmmss").format(new Date())));
 			params.add(new Parameter(Parameter.USER_DATA, new StringUserData("hello, world").getBody()));
 			Packet p = new Packet(Packet.OP_DELIVER_MESSAGE, params.toArray(new Parameter[]{}));

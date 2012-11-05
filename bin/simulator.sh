933a55857854b4726586b9ebb43c16853a8dde05
marko asplund <marko.asplund@ixonos.com>
2012-11-05 21:43:44 +0200

    add config parameter examples in startup script; configure file logging instead of console

--- bin/simulator.sh
+++ bin/simulator.sh
@@ -5,5 +5,13 @@ msg_port=${CIMD_MSG_PORT:-9072}
 
 libs=`ls lib/*.jar|awk '{ ORS=":" } { print $1; }'`
 
-# -Dcimdsimulator.messageInjectSleepTimeMillis=5
-java -cp $libs -Dcimdsimulator.port=$port -Dcimdsimulator.messagePort=$msg_port com.ixonos.cimd.simulator.CIMDSimulator
+# JVM remote debugging
+#DEBUG=-agentlib:jdwp=transport=dt_socket,server=y,address=localhost:7070,suspend=n
+
+# configuration parameters:
+# -Dcimdsimulator.port=9071                         # mandatory
+# -Dcimdsimulator.messagePort=9072                  # mandatory
+# -Dcimdsimulator.messageInjectSleepTimeMillis=20   # optional
+# -Dcimdsimulator.useCimdCheckSum=false             # optional
+
+java $DEBUG -cp $libs -Dcimdsimulator.port=$port -Dcimdsimulator.messagePort=$msg_port com.ixonos.cimd.simulator.CIMDSimulator

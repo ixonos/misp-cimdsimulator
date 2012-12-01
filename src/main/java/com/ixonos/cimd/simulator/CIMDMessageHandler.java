d5ae168fee50ae796b281ec6660edacbce33f79e
marko asplund <marko.asplund@ixonos.com>
2012-12-01 23:42:18 +0200

    add code for decoding CIMD packet user data (work in progress)

--- src/main/java/com/ixonos/cimd/simulator/CIMDMessageHandler.java
+++ src/main/java/com/ixonos/cimd/simulator/CIMDMessageHandler.java
@@ -11,9 +11,16 @@
  */
 package com.ixonos.cimd.simulator;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
 import org.slf4j.Logger;
@@ -61,18 +68,14 @@ public class CIMDMessageHandler extends IoHandlerAdapter {
       break;
     case Packet.OP_SUBMIT_MESSAGE:
       // message from application to SMSC
-      Parameter dest = null;
-      for(Parameter pr : req.getParameters()) {
-        if(Parameter.DESTINATION_ADDRESS == pr.getNumber())
-          dest = pr;
-          break;
-      }
-      if(dest == null) {
+      CIMDPacket cp = new CIMDPacket(req);
+      if(cp.getDestinationAddress() == null) {
         logger.error("destination parameter missing: "+req);
         return;
       }
-      res = new Packet(req.getOperationCode() + 50, req.getSequenceNumber(), dest,
-          new Parameter(60, new SimpleDateFormat("yyMMddHHmmss").format(new Date())));
+      res = new Packet(req.getOperationCode() + 50, req.getSequenceNumber(),
+        new Parameter(Parameter.DESTINATION_ADDRESS, cp.getDestinationAddress()),
+        new Parameter(Parameter.MC_TIMESTAMP, new SimpleDateFormat("yyMMddHHmmss").format(new Date())));
       break;
     case Packet.OP_ALIVE:
       res = new Packet(req.getOperationCode() + 50, req.getSequenceNumber());
@@ -105,6 +108,64 @@ public class CIMDMessageHandler extends IoHandlerAdapter {
   public void sessionClosed(IoSession session) throws Exception {
     super.sessionClosed(session);
     logger.debug("session closed: " + session.getId());
+  }
+
+  public static class CIMDPacket {
+    private String destinationAddress;
+    private String userData; // user data un-encoded
+    private static final Charset charset = Charset.forName("GSM");
+
+    public CIMDPacket(Packet p) {
+      this.parsePacket(p);
+    }
+
+    private void parsePacket(Packet p) {
+      Integer dataCodingScheme = null;
+      for(Parameter pr : p.getParameters()) {
+        switch (pr.getNumber()) {
+          case Parameter.DESTINATION_ADDRESS:
+            destinationAddress = pr.getValue();
+            break;
+          case Parameter.DATA_CODING_SCHEME:
+            dataCodingScheme = Integer.valueOf(pr.getValue());
+            break;
+          case Parameter.USER_DATA:
+            userData = pr.getValue();
+            break;
+          case Parameter.USER_DATA_BINARY:
+            userData = decodeUserDataBinary(dataCodingScheme, pr.getValue());
+        }
+      }
+    }
+
+    private String decodeUserDataBinary(Integer dataCodingScheme, String userDataBinary) {
+      if(dataCodingScheme == null) {
+        logger.info("data coding scheme not specified for user binary data");
+        return userDataBinary;
+      }
+      if(dataCodingScheme == 0) {
+        CharBuffer cb = null;
+        try {
+          CharsetDecoder decoder = charset.newDecoder();
+          byte[] encHex = Hex.decodeHex(userDataBinary.toCharArray());
+          cb = decoder.decode(ByteBuffer.wrap(encHex));
+        } catch (DecoderException | CharacterCodingException e) {
+          logger.info("failed to decode user data: "+userDataBinary, e);
+        }
+        return new String(cb.array());
+      } else {
+        logger.info("Unsupported data coding scheme: "+dataCodingScheme);
+      }
+      return userDataBinary;
+    }
+
+    public String getDestinationAddress() {
+      return destinationAddress;
+    }
+
+    public String getUserData() {
+      return userData;
+    }
   }
 
 }
